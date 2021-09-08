f7fb882c5556737c730915d83d57a9f01c0a6b51
Igor Petrov <i.petrov@bintime.com>
2021-09-08 13:02:45 +0300

    Add local configs under the git

---
+++ common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=mysql;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => 'verysecret',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
